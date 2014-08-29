6e3e17f7b7000663fac7a770a1969d43ef2cd028
Lance Ennen <byennen@gmail.com>
2014-08-29 15:59:50 -0500

    setup iron routing

---
+++ client/views/layouts/application.js
@@ -0,0 +1,33 @@
+
+/*****************************************************************************/
+/* Application: Event Handlers and Helpersss .js*/
+/*****************************************************************************/
+Template.Application.events({
+  /*
+   * Example:
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.Application.helpers({
+  /*
+   * Example:
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* Application: Lifecycle Hooks */
+/*****************************************************************************/
+Template.Application.created = function () {
+};
+
+Template.Application.rendered = function () {
+};
+
+Template.Application.destroyed = function () {
+};
