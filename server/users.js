96ef6208a8f8483901e7d4395d6afa25c55f45f8
Lance Ennen <byennen@gmail.com>
2014-09-03 11:43:26 -0500

    add profile inputs

---
+++ server/users.js
@@ -0,0 +1,9 @@
+//TODO: save user to users collection
+//
+// Accounts.onCreateUser(function(options, user){
+//   var userProperties = {
+//     profile: options.profile || {},
+//   };
+//   user = _.extend(user, userProperties);
+//   return user;
+// });
