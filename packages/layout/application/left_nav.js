fa228258887b6f64623cfd7f7329e5c090624dcc
Lance Ennen <byennen@gmail.com>
2014-10-04 22:48:56 -0500

    refactor left nav to use velocityjs

--- packages/layout/application/left_nav.js
+++ packages/layout/application/left_nav.js
@@ -6,9 +6,7 @@
     var settings = $.extend({
       'menu': ('#left_nav'),
       'push': ('.push'),
-      'side': 'left',
       'menuWidth': '22em',
-      'speed': '300',
       'activeBtn':'menu-open'
     }, options);
 
@@ -17,42 +15,20 @@
       push = $(settings.push),
       width = settings.menuWidth;
 
-    var positionOffScreen = {
-      'position': 'fixed',
-      'top': '0',
-      'bottom': '0',
-      'settings.side': '-' + settings.menuWidth,
-      'width': settings.menuWidth,
-      'height': '100%'
-    };
-
-    var animateSlide = {
-      '-webkit-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      '-moz-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      '-ms-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      '-o-transition': settings.side + ' ' + settings.speed + 'ms ease',
-      'transition': settings.side + ' ' + settings.speed + 'ms ease'
-    };
-
-    menu.css(positionOffScreen);
-    push.css(settings.side, '0');
-    menu.css(animateSlide);
-    push.css(animateSlide);
-
     menu._state = 'closed';
 
     menu.open = function() {
       menu._state = 'open';
-      menu.css(settings.side, '0');
-      push.css(settings.side, width);
       menuLink.addClass(settings.activeBtn);
+      push.velocity({ left: width }, {easing: [100, 0]});
+      menu.velocity({ left: 0 }, {easing: [100, 0]});
     };
 
     menu.close = function() {
       menu._state = 'closed';
-      menu.css(settings.side, '-' + width);
-      push.css(settings.side, '0');
       menuLink.removeClass(settings.activeBtn);
+      menu.velocity({ left: '-' + width }, {easing: [100, 0]});
+      push.velocity({ left: 0 }, {easing: [100, 0]});
     };
 
     $(document).on('click.leftNav', function(e) {
