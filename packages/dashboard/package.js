985a61626d65d54f9fd9816499500e97f8317298
Lance Ennen <byennen@gmail.com>
2014-10-15 10:27:17 -0400

    refactor clean up of dashboard

--- packages/dashboard/package.js
+++ packages/dashboard/package.js
@@ -4,5 +4,12 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.use(['templating', 'less'], 'web');
-  api.addFiles(['dashboard.html', 'dashboard.js', 'dashboard.less'], 'web');
+  api.addFiles(['dashboard.html',
+                'dashboard.js',
+                'dashboard.less',
+                '_feed.html',
+                '_board.html',
+                '_banner.html',
+                '_documents.html'
+                ], 'web');
 });
