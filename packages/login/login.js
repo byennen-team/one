48e37371acce07d3bae7dd9bb16929c3ca64b1a1
Lance Ennen <byennen@gmail.com>
2014-10-20 11:05:48 -0400

    for demo redirect login to dashboard

--- packages/login/login.js
+++ packages/login/login.js
@@ -1,11 +1,11 @@
 Template.login.events({
   'submit form': function (event, template) {
     event.preventDefault();
-
-    var agentId = template.find('#agentID').value;
-    agentId = parseInt(agentId);
-    Meteor.loginWithElliman(agentId, function (error) {
-      // XXX error handling
-    });
+     Router.go('dashboard');
+    // var agentId = template.find('#agentID').value;
+    // agentId = parseInt(agentId);
+    // Meteor.loginWithElliman(agentId, function (error) {
+    //   // XXX error handling
+    // });
   }
 });
