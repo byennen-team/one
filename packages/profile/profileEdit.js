392ac7eda10785daea33c691695690414826949a
Lance Ennen <byennen@gmail.com>
2014-10-31 15:31:20 -0400

    redirect after saving profile

--- packages/profile/profileEdit.js
+++ packages/profile/profileEdit.js
@@ -3,29 +3,7 @@ Template.profileEdit.events({
     event.preventDefault();
     data = SimpleForm.processForm(event.target);
     combinedData = _.extend(Meteor.user().profile, data);
-    Meteor.users.update({_id: Meteor.userId()},{$set: {profile: combinedData}})
+    Meteor.users.update({_id: Meteor.userId()},{$set: {profile: combinedData}});
+    Router.go('dashboard');
   }
 });
-
-
-// Template.profileEdit.events({
-//   'submit form' : function(event, template) {
-//     event.preventDefault();
-//
-//     name = template.find("input[name=name]");
-//     phone = template.find("input[name=phone]");
-//     console.log('click');
-//
-//     // Do form validation
-//
-//     var data = {
-//       name: name.value,
-//       phone: phone.value,
-//     };
-//
-//     name.value="";
-//     phone.value="";
-//
-//   Profile.insert(data);
-//
-// }});
