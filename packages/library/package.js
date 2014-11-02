16514a9891833a17cf254b818220ca72fd8c146b
Lance Ennen <byennen@gmail.com>
2014-11-02 09:47:06 -0500

    first pass at s3 file upload library

--- packages/library/package.js
+++ packages/library/package.js
@@ -4,6 +4,11 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
+  var both = ['web', 'server'];
+
+  api.use('lepozepo:s3', both);
+  api.addFiles('s3.js', 'server');
+
   api.use(['templating', 'less'], 'web');
-  api.addFiles(['library.html', 'library.js', 'library.less'], 'web');
+  api.addFiles(['library.html', 'library.js', 'library.less', 'documentUploader.html', 'documentUploader.js'], 'web');
 });
