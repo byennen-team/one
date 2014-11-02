c0bd8e78a6df9bdcdd23121017da32e8be79655b
Lance Ennen <byennen@gmail.com>
2014-11-01 21:51:55 -0400

    upload user profile image and save

--- packages/profile/profileUploader.js
+++ packages/profile/profileUploader.js
@@ -2,7 +2,9 @@ Template.profileUploader.events({
   "click button.upload": function(){
     var files = $("input.file_bag")[0].files
     S3.upload(files,"/profile-images/",function(e,r){
-        console.log(r);
+      console.log(r.url);
+      // TODO: resize image
+      Meteor.users.update({_id: Meteor.userId()},{$set:{profile: {photoUrl: r.url}}})
     });
   }
 });
