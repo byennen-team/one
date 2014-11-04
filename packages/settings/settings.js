acd7c1a87a68be5b4920a8d2f6dba93990df3627
Lance Ennen <byennen@gmail.com>
2014-11-04 10:29:41 -0500

    fix bacis auth for staging

--- packages/settings/settings.js
+++ packages/settings/settings.js
@@ -1,5 +1,7 @@
-// Settings = {};
-//
-// Settings.isDevelopment = Meteor.settings.public.ENVIRONMENT === 'development';
-//
-// Settings.isProduction = Meteor.settings.public.ENVIRONMENT === 'production';
+Settings = {};
+
+Settings.isDevelopment = Meteor.settings.public.ENVIRONMENT === 'development';
+
+Settings.isStaging = Meteor.settings.public.ENVIRONMENT === 'staging';
+
+Settings.isProduction = Meteor.settings.public.ENVIRONMENT === 'production';
