18fac0b0fe8182fcbc8dd86893091658e97ead70
Lance Ennen <byennen@gmail.com>
2014-11-06 08:26:24 -0500

    fixed top nav

--- packages/layout/package.js
+++ packages/layout/package.js
@@ -3,6 +3,8 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.use(['templating', 'authorization', 'left-nav'], 'web');
-  api.addFiles(['_footer.html', '_header.html', 'application.html', 'application.js', 'application.css'], 'web');
+  api.use(['templating', 'authorization', 'left-nav', 'less'], 'web');
+  api.addFiles(['_footer.html', '_header.html', 'application.html',
+                'application.js', 'application.less'
+               ], 'web');
 });
