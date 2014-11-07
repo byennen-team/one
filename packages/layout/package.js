521f60aeefd292b030262e507c82e1b424437d6c
Lance Ennen <byennen@gmail.com>
2014-11-07 11:31:24 -0500

    add load more link to directory

--- packages/layout/package.js
+++ packages/layout/package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.use(['templating', 'authorization', 'left-nav', 'less'], 'web');
-  api.addFiles(['_footer.html', '_header.html', 'application.html',
-                'application_client.js', 'application.less'
+  api.addFiles(['_header.html', 'application.html', 'application_client.js',
+                'application.less'
                ], 'web');
 });
