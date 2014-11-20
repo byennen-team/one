feac55e580b39e236d4eedf79f98d7c146c1528b
Lance Ennen <byennen@gmail.com>
2014-11-20 11:26:44 -0500

    finish documents drop down

--- packages/documents-menu/documents_menu_client.js
+++ packages/documents-menu/documents_menu_client.js
@@ -1,25 +1,18 @@
 Template.documentsMenu.helpers({
   current_page: function () {
-    var current_page = Routes.getName() === Routes.MY_DOCUMENTS ? 'My Documents' : 'Company Documents';
-    return current_page;
-  },
-  current_url: function () {
-    var current_url = Routes.getName() === Routes.MY_DOCUMENTS ? 'myDocuments' : 'companyDocuments';
-    return current_url;
-  },
-  next_url: function () {
-    var next_url = Routes.getName() === Routes.MY_DOCUMENTS ? 'companyDocuments' : 'myDocuments';
-    return next_url;
+    if (Routes.getName() === Routes.MY_DOCUMENTS) {
+      return "<a href='/documents'>My Documents <i class='fa fa-sort-desc'></i></a>";
+    } else {
+      return "<a href='/documents/company'><img src='../../images/documents/DE-white.png'/> Company Documents <i class='fa fa-sort-desc'></i></a>"
+    }
   },
   next_page: function () {
-    var next_page = Routes.getName() === Routes.MY_DOCUMENTS ? 'Company Documents' : 'My Documents';
-    return next_page;
-  },
-  // icon: function () {
-  //   if (Routes.getName() === Routes.COMPANY_DOCUMENTS) {
-  //     return "<p>All new content for </p>";
-  //   }
-  // }
+    if (Routes.getName() === Routes.MY_DOCUMENTS) {
+      return "<a href='/documents/company'><img src='../../images/documents/DE-white.png'/> Company Documents</a>";
+    } else {
+      return "<a href='/documents'>My Documents</a>"
+    }
+  }
 });
 
 Template.documentsMenu.events({
