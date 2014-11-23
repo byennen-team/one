eddfe1a083a8bb365eead58c8b4cbc3ba42bc00c
Lance Ennen <byennen@gmail.com>
2014-11-23 01:44:47 -0500

    css clean up

--- packages/layout/package.js
+++ packages/layout/package.js
@@ -5,6 +5,7 @@ Package.describe({
 Package.onUse(function (api) {
   api.use(['templating', 'authorization', 'left-nav', 'less'], 'web');
   api.addFiles(['application.html', 'application_client.js', 'application.less',
-                'buttons.less', 'colors.less', 'cleanup.less'
+                'buttons.less', 'colors.less', 'padding_and_margin.less',
+                'cleanup.less'
                ], 'web');
 });
