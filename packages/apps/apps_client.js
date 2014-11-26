544330d658e6f2ab2fcb5a16e261487e0d3f29c9
Lance Ennen <byennen@gmail.com>
2014-11-26 10:44:30 -0500

    add new apps page minus css shadows

--- packages/apps/apps_client.js
+++ packages/apps/apps_client.js
@@ -1,6 +1,2 @@
 Template.apps.events({
-  'click .close-feature-app': function (event, template) {
-    $('#featured-apps').velocity({ height: 0 }, "easeInSine");
-    $('.featured-app-image, .close-feature-app').hide();
-  }
 });
