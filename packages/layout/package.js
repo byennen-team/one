6b04dd4f914438d7e36e671fbe024980370ca7eb
Lance Ennen <byennen@gmail.com>
2014-12-05 13:05:19 -0500

    require styles package in layouts package

--- packages/layout/package.js
+++ packages/layout/package.js
@@ -3,6 +3,6 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.use(['templating', 'authorization', 'left-nav', 'less'], 'web');
+  api.use(['templating', 'authorization', 'left-nav', 'less', 'styles'], 'web');
   api.addFiles(['application.html', 'application_client.js'], 'web');
 });
