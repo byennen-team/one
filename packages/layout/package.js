fc0f9fa0c76e035c2e447f4c7b490421ef43ada4
Lance Ennen <byennen@gmail.com>
2014-12-10 16:04:15 -0500

    refector navbar and left-nav into layout package

--- packages/layout/package.js
+++ packages/layout/package.js
@@ -3,6 +3,15 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.use(['templating', 'authorization', 'left-nav', 'less', 'styles'], 'web');
-  api.addFiles(['application.html', 'application_client.js'], 'web');
+  api.use([
+    'templating', 'authorization', 'less',
+    'styles', 'jquery', 'stevezhu:velocity.js@0.1.0'
+    ], 'web');
+
+  api.addFiles([
+    'application.html', 'application_client.js',
+    'navbar.html', 'navbar_client.js', 'navbar.less',
+    'left_nav.html', 'leftNav.js', 'left_nav_client.js',
+    'left_nav.less'
+    ], 'web');
 });
--- packages/navbar/package.js
+++
@@ -1,9 +0,0 @@
-Package.describe({
-  summary: 'NavBar'
-});
-
-Package.onUse(function (api) {
-  api.use(['templating', 'less'], 'web');
-  api.addFiles(['navbar.html', 'navbar_client.js', 'navbar.less'
-               ], 'web');
-});
