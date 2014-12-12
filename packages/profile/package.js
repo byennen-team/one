644dad4b6ce265304785068a04b523a2c5b1dca2
Lance Ennen <byennen@gmail.com>
2014-12-12 11:05:50 -0500

    refactor profile header, setup profile property search

--- packages/profile/package.js
+++ packages/profile/package.js
@@ -17,7 +17,8 @@ Package.onUse(function (api) {
     'profile_gallery.html', 'profile_gallery.less', 'gallery_modal.html',
     'gallery_modal.less',
     'profile_header.html', 'profile_header.less', 'profile_header.js',
-    'profile_property_search.html', 'profile_testimonial.html'
+    'profile_property_search.html', 'profile_property_search.less',
+    'profile_testimonial.html'
     ], 'web');
 
   api.export('Profile', 'web');
