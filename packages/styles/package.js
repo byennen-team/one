8f8fbf1db84ce241c8f9f284e9c3c07d8e81150a
Dave Bascom <dave@cumulocreative.com>
2014-12-16 14:41:39 -0500

    less fixes

--- packages/styles/package.js
+++ packages/styles/package.js
@@ -6,7 +6,8 @@ Package.onUse(function (api) {
   api.use(['templating', 'less'], 'web');
 
   api.addFiles(['main.less', 'buttons.less', 'colors.less',
-                'padding_and_margin.less', 'cleanup.less', 'go_one_icons.less'
+                'padding_and_margin.less', 'cleanup.less', 'go_one_icons.less',
+                'form.less'
                ], 'web');
 
   //style guide
