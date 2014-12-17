3538e90c295c079c1c1f5400ad378d193187d2b9
Dave Bascom <dave@cumulocreative.com>
2014-12-17 08:21:48 -0500

    LESS cleanup - profile package

--- packages/styles/package.js
+++ packages/styles/package.js
@@ -5,9 +5,9 @@ Package.describe({
 Package.onUse(function (api) {
   api.use(['templating', 'less'], 'web');
 
-  api.addFiles(['main.less', 'buttons.less', 'colors.less',
+  api.addFiles(['main.less', 'one_strap.less', 'buttons.less', 'colors.less',
                 'padding_and_margin.less', 'cleanup.less', 'go_one_icons.less',
-                'form.less'
+                'form.less', 'typography.less'
                ], 'web');
 
   //style guide
