b32dba694f7b189646b0d90a9ab66408b7e3b998
Lance Ennen <byennen@gmail.com>
2014-12-24 15:54:58 -0500

    one-profile package --- restructing. @jperl @DaveSauce thoughts on structing the packages like this as they grow? Happy Holidays guys!

--- packages/one-profile/package.js
+++ packages/one-profile/package.js
@@ -3,27 +3,71 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.use(['templating', 'less', 'reactive-var', 'jquery', 'stevezhu:velocity.js@0.1.0'], 'web');
+  api.use([
+    'templating',
+    'less',
+    'reactive-var',
+    'jquery',
+    'stevezhu:velocity.js@0.1.0'
+    ], 'web');
 
+  // less
   api.addFiles([
-    'profile.js', 'profile_about.html', 'profile_about.less',
-    'profile.html', 'profile_client.js', 'profile.less',
-    'profile_card.html', 'profile_card.js', 'profile_card.less',
-    'profile_nav.html', 'profile_nav.less', 'profile_nav.js',
-    'profile_activity.html', 'profile_activity.less', 'profile_activity.js',
-    'profile_banner.html', 'profile_banner_client.js', 'profile_banner.less',
-    'profile_followers.html', 'profile_followers.less', 'profile_followers.js',
-    'profile_following.html', 'profile_following.js',
-    'profile_gallery.html', 'profile_gallery.less', 'gallery_modal.html',
-    'gallery_modal.less',
-    'profile_header.html', 'profile_header.less', 'profile_header.js',
-    'profile_property_search.html', 'profile_property_search.less', 'profile_property_search_client.js',
-    'profile_testimonial.html', 'profile_testimonial.less',
-    'profile_buyer.html', 'profile_buyer.less',
-    'profile_contact.html', 'profile_contact.less',
-    'profile_footer.html', 'profile_footer.less',
-    'modals/profile_header_image_modal.html', 'modals/profile_header_image_modal.js', 'modals/profile_header_delete_image_modal.html',
-    'modals/profile_contact_message_success_modal.html', 'modals/profile_contact_message_failed_modal.html', 'modals/profile_activity_link_modal.html'
+    'lib/client/less/profile_about.less',
+    'lib/client/less/profile.less',
+    'lib/client/less/profile_card.less',
+    'lib/client/less/profile_nav.less',
+    'lib/client/less/profile_activity.less',
+    'lib/client/less/profile_banner.less',
+    'lib/client/less/profile_followers.less',
+    'lib/client/less/profile_gallery.less',
+    'lib/client/less/gallery_modal.less',
+    'lib/client/less/profile_header.less',
+    'lib/client/less/profile_property_search.less',
+    'lib/client/less/profile_testimonial.less',
+    'lib/client/less/profile_buyer.less',
+    'lib/client/less/profile_contact.less',
+    'lib/client/less/profile_footer.less'
+  ], 'web');
+
+  // modals
+  api.addFiles([
+    'lib/client/modals/profile_header_image_modal.html',
+    'lib/client/modals/profile_header_image_modal.js',
+    'lib/client/modals/profile_header_delete_image_modal.html',
+    'lib/client/modals/profile_contact_message_success_modal.html',
+    'lib/client/modals/profile_contact_message_failed_modal.html',
+    'lib/client/modals/profile_activity_link_modal.html',
+    'lib/client/modals/gallery_modal.html'
+    ], 'web');
+
+  // templates
+  api.addFiles([
+    'lib/client/templates/profile.js',
+    'lib/client/templates/profile_about.html',
+    'lib/client/templates/profile.html',
+    'lib/client/templates/profile_client.js',
+    'lib/client/templates/profile_card.html',
+    'lib/client/templates/profile_card.js',
+    'lib/client/templates/profile_nav.html',
+    'lib/client/templates/profile_nav.js',
+    'lib/client/templates/profile_activity.html',
+    'lib/client/templates/profile_activity.js',
+    'lib/client/templates/profile_banner.html',
+    'lib/client/templates/profile_banner_client.js',
+    'lib/client/templates/profile_followers.html',
+    'lib/client/templates/profile_followers.js',
+    'lib/client/templates/profile_following.html',
+    'lib/client/templates/profile_following.js',
+    'lib/client/templates/profile_gallery.html',
+    'lib/client/templates/profile_header.html',
+    'lib/client/templates/profile_header.js',
+    'lib/client/templates/profile_property_search.html',
+    'lib/client/templates/profile_property_search_client.js',
+    'lib/client/templates/profile_testimonial.html',
+    'lib/client/templates/profile_buyer.html',
+    'lib/client/templates/profile_contact.html',
+    'lib/client/templates/profile_footer.html'
     ], 'web');
 
   api.export('Profile', 'web');
