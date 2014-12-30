e80673d628f89d9e02718a522f63c6c84c564da0
Dave Bascom <dave@cumulocreative.com>
2014-12-30 06:33:03 -0500

    installed less variables and functions (add @import 'var.import.less'; to any less file to use variables)

--- packages/styles/package.js
+++ packages/styles/package.js
@@ -5,9 +5,10 @@ Package.describe({
 Package.onUse(function (api) {
   api.use(['templating', 'less'], 'web');
 
-  api.addFiles(['colors.less', 'main.less', 'one_strap.less', 'buttons.less', 
-                'padding_and_margin.less', 'cleanup.less', 'go_one_icons.less',
-                'form.less', 'typography.less', 'modals.less'
+  api.addFiles(['colors.less', 'main.less', 'one_strap.less', 
+  							'buttons.less', 'padding_and_margin.less', 'cleanup.less', 
+  							'go_one_icons.less', 'form.less', 'typography.less', 
+                'modals.less'
                ], 'web');
 
   //style guide
