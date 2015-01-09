ed8dbad8efc10c308780abb7d0428c8dd2aa0ee6
Chuck Short <chuck@gooneapp.com>
2015-01-09 16:08:08 +0000

    terarlly whatever

--- packages/files/package.js
+++ packages/files/package.js
@@ -6,12 +6,12 @@ Package.onUse(function (api) {
   var both = ['web', 'server'];
 
   api.use('aldeed:simple-schema', both);
-
-  api.use('aws-sdk', 'server');
+ 
+  api.use(['aws-sdk', 'npm-container'], 'server');
 
   api.addFiles(['files.js', 'folder.js'], both);
-  api.addFiles('files_server.js', 'server');
+  api.addFiles(['files_server.js', 'file_resize_server.js'], 'server');
   api.addFiles('files_client.js', 'web');
-
+  
   api.export(['Files', 'FileTools', 'Folder'], both);
 });
