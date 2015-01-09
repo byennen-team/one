d6dc2eb9b76f54c3a326e2f3b2e078f8592003e0
Chuck Short <chuck@gooneapp.com>
2015-01-09 18:27:02 +0000

    sync chucks work on profile image resizing on elliman_production.json start up data to s3

--- packages/aws-sdk/aws.js
+++ packages/aws-sdk/aws.js
@@ -4,7 +4,6 @@ AWS.config.apiVersions = {
   ec2: '2014-09-01',
   s3: '2006-03-01'
 };
-
 AWS.config.update({
   accessKeyId: Meteor.settings.AWS_ACCESS_KEY_ID,
   secretAccessKey: Meteor.settings.AWS_SECRET_ACCESS_KEY
