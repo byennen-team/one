f9df78521ddbc59abe94081d6faec459ef46b9f1
Chuck Short <chuck@gooneapp.com>
2015-01-11 16:55:10 +0000

    clean up chucks branch

--- packages/files/package.js
+++ packages/files/package.js
@@ -7,10 +7,10 @@ Package.onUse(function (api) {
 
   api.use('aldeed:simple-schema', both);
  
-  api.use(['aws-sdk', 'npm-container'], 'server');
+  api.use(['aws-sdk', 'meteorhacks:npm'], 'server');
 
   api.addFiles(['files.js', 'folder.js'], both);
-  api.addFiles(['files_server.js', 'file_resize_server.js'], 'server');
+  api.addFiles(['files_server.js', 'file_resize_server.js', 'packages.json'], 'server');
   api.addFiles('files_client.js', 'web');
   
   api.export(['Files', 'FileTools', 'Folder'], both);
