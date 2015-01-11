cadd6d05f28159c00f71bf8bba3b59f12b2d8d1e
Chuck Short <chuck@gooneapp.com>
2015-01-11 22:50:39 +0000

    add dir, now some files timeout during upload

--- packages/files/file_resize_server.js
+++ packages/files/file_resize_server.js
@@ -13,53 +13,50 @@ resizeWidths = { "mobile_":480, "thumb_":200, "full_":1200 },
 base = process.env.PWD
 var imagetmp = 'imagetmp.jpeg'
 var imageremote = 'remoteimage.jpeg'
-FileTools.fetch_resize_and_upload = function(url) {
+FileTools.fetch_resize_and_upload = function(url, id, next) {
         console.log('fetch_resize_and_upload', url);
         var originalName = url.substring(url.lastIndexOf('/')+1)
         var imageFile = base+'/'+imagetmp;
         request(url).on('end', function(error, response, body){
           console.log('response end');
-          resize_and_upload(imagetmp, originalName);
+          resize_and_upload(imagetmp, originalName, id, next);
         }).pipe(fs.createWriteStream(imageFile))
 }
-var resize_and_upload = function(imageFile, originalName) {
-  console.log('resize_and_upload: ', imageFile, originalName);
+var resize_and_upload = function(imageFile, originalName, id, next) {
+    console.log('resize_and_upload: ', imageFile, originalName, id);
+    var remoteFolder = id+'/'
+    var remotefilename = originalName;
     Meteor.wrapAsync(im.resize({
       srcPath: base+'/'+imageFile,
       dstPath: base+'/full_'+imageFile,
       width:   resizeWidths['full_'],
       quality: 0.6
     }))
+    uploadToS3(base+'/full_'+ imageFile, remoteFolder+'full_'+remotefilename)
     Meteor.wrapAsync(im.resize({
         srcPath: base+'/'+imageFile,
         dstPath: base+'/mobile_'+imageFile,
         width:   resizeWidths['mobile_'],
         quality: 0.6
       }))
+    uploadToS3(base+'/mobile_'+imageFile, remoteFolder+'mobile_'+remotefilename)
     Meteor.wrapAsync(im.resize({
           srcPath: base+'/'+imageFile,
           dstPath: base+'/thumb_'+imageFile,
           width:   resizeWidths['thumb_'],
           quality: 0.6
         }))
-    uploadAll(imageFile, originalName)
-
+    uploadToS3(base+'/thumb_'+imageFile, remoteFolder+'thumb_'+remotefilename)
+    return next
 }
 
-var uploadAll = function(imagefile, remotefilename) {
-  console.log('upload all:', imagefile, ' to: ', remotefilename)
-  uploadToS3(base+'/mobile_'+imagefile, 'mobile_'+remotefilename)
-  uploadToS3(base+'/thumb_'+imagefile, 'thumb_'+remotefilename)
-  uploadToS3(base+'/full_'+ imagefile, 'full_'+remotefilename)
-}
 var uploadBucket = new AWS.S3()
 var uploadToS3 = function uploadToS3(localfile, remotefilename){
-    var signedFile = FileTools.signUpload(localfile, 'private', 'image/jpeg')
-    console.log('signed-remoteFile', signedFile.credentials.signature);
+    //var signedFile = FileTools.signUpload(localfile, 'private', 'image/jpeg')
+    //console.log('signed-remoteFile', signedFile.credentials.signature);
     var stream = fs.createReadStream(localfile)
-    var s3bucket = Meteor.wrapAsync(uploadBucket)
     uploadBucket.createBucket(function() {
-      var params = { Bucket: 'chuck.goone', Key: remotefilename, Body: stream };
+      var params = { Bucket: Meteor.settings.AWS_BUCKET, Key: remotefilename, Body: stream };
       uploadBucket.putObject(params, function(err, data) {
         if (err) {
           console.log("Error uploading data: ", err);
