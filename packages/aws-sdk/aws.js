f2b7dfac74aa97884c8c9ec4ce23f256e0896d71
Chuck Short <chuck.short.smart@gmail.com>
2015-01-13 15:06:26 -0500

    AWS_REGION a Meteor.settings

--- packages/aws-sdk/aws.js
+++ packages/aws-sdk/aws.js
@@ -1,5 +1,5 @@
 AWS = Npm.require('aws-sdk');
-AWS.config.region = 'us-west-2';
+AWS.config.region = Meteor.settings.AWS_REGION;
 AWS.config.update({
   accessKeyId: Meteor.settings.AWS_ACCESS_KEY_ID,
   secretAccessKey: Meteor.settings.AWS_SECRET_ACCESS_KEY
