69eaa0e3b3dafea1e9dbc52b88e7d0ad513def23
Lance Ennen <byennen@gmail.com>
2015-01-13 10:34:31 -0500

    adding beta.gooneapp.com

--- packages/settings/settings.js
+++ packages/settings/settings.js
@@ -4,4 +4,6 @@ Settings.isDevelopment = Meteor.settings.public.ENVIRONMENT === 'development';
 
 Settings.isStaging = Meteor.settings.public.ENVIRONMENT === 'staging';
 
+Settings.isBeta = Meteor.settings.public.ENVIRONMENT === 'beta';
+
 Settings.isProduction = Meteor.settings.public.ENVIRONMENT === 'production';
