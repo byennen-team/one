aa89676001cd8c703bcf39460792ca7a65fbf47e
Andreas Fruth <andreas.fruth@gmail.com>
2015-01-17 10:30:01 +0200

    Notifications package first pass
    Most interactions are here, needs front-end integration.

---
+++ packages/notifications/notifications-server.js
@@ -0,0 +1,101 @@
+Meteor.publish('notifications', function(limit) {
+	if (limit)
+		check(limit, Number);
+	else
+		limit = 20;
+
+	return Notifications.find({createdFor: userId},{limit: limit});
+})
+
+Meteor.methods({
+	addNotification: function(toUserId,message,link) {
+		check(toUserId,String);
+		check(message,String);
+
+		if (link)
+			check(link, String);
+
+		//check that the user exists
+		var user = Meteor.users.findOne(toUserId);
+
+		if(!user)
+			return ("User does not exist");
+
+		var options = {
+			createdAt: new Date(),
+			createdFor: toUserId,
+			notificationText: message
+		};
+
+		if (link)
+			options.link = link;
+
+		Notifications.insert(options, function(error, result) {
+			if (error)
+				return (error.message);
+			else
+				return (null, result);
+		})
+	},
+	markNotificationAsRead: function(notificationId) {
+		check(notificationId, String);
+
+		if (!this.userId)
+			return "You are not logged in";
+
+		//check that the notification exist
+		var notification = Notifications.findOne(notificationId);
+
+		if (!notification)
+			return "Notification does not exist";
+
+		if (notification.read === true)
+			return "Notification already read";
+
+		Notifications.update(notificationId, {$set: {
+			read: true
+		}}, function(error, result) {
+			if (error)
+				return "Could not update notification";
+
+			return result;
+		});
+
+	}
+});
+
+Notify.messages = {
+	FOLLOWED_BY_USER: '{arg1} followed you',
+	DOCUMENT_SHARED_WITH_YOU: '{arg1} shared a document with you',
+	ADDED_TO_A_TEAM: '{arg1} added you to team {arg2}'
+};
+
+/**
+*	@description: Function that generates a message from a string and multiple arguments
+*	@params: text - the string to replace in, arg0...argN - the strings to replace with
+*	@returns: String text
+*/
+Notify.generateMessageText = function(text, stringArray) {
+
+	//checking if the number of arguments passed is same with the number of string placeholders
+	var placeholders = /(\{arg\d\})/gi;
+	var found = text.match(placeholders);
+
+	if (stringArray.length != found.length)
+		throw new Meteor.Error(500,"You have specified a different number of arguments. (Notifications package)");
+
+	for (var i=0;i<stringArray.length;i++) {
+		var argNo = i+1;
+		text = text.replace('{arg'+argNo+'}',stringArray[i])
+	};
+
+	return text;
+};
+
+
+Notify.addNotification = function(toUserId,message,link) {
+	Meteor.call('addNotification',toUserId,message, function(error, result) {
+		if (error)
+			throw new Meteor.Error(500,error);
+	});
+};
--- packages/notifications/package.js
+++ packages/notifications/package.js
@@ -1,16 +1,23 @@
 Package.describe({
-  summary: " \* Fill me in! *\ ",
-  version: "1.0.0",
-  git: " \* Fill me in! *\ "
+  summary: " Notifications package ",
+  version: "1.0.0"
 });
-
+var both = ['server','web'];
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.3.1');
-  api.addFiles('notifications.js');
+	api.use(['aldeed:simple-schema'], both);	
+	api.versionsFrom('METEOR@0.9.3.1');
+	api.addFiles('notifications.js', ['server', 'web']);
+	
+	api.addFiles('notifications-server.js','server');
+
+	api.addFiles('notifications-client.js', 'web');
+  
+
+	api.export(['Notifications', 'Notify'], both);
 });
 
 Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('notifications');
-  api.addFiles('notifications-tests.js');
+	api.use('tinytest');
+	api.use('notifications');
+	api.addFiles('notifications-tests.js');
 });
