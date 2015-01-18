c7f74bd16098cf1f801eb032aa748b74c1bc99c5
Lance Ennen <byennen@gmail.com>
2015-01-18 17:19:51 -0500

    rabase off master to fix region issues with s3

--- packages/users-elliman/populate_agents.js
+++ packages/users-elliman/populate_agents.js
@@ -55,7 +55,7 @@ var q_fetch_resize_and_upload = function(user){
   var xpat = /\.([0-9a-z]+)(?:[\?#]|$)/i;
   var _URL = user.profile.photoUrl;
   var _ext = _URL.match(xpat)[0];
-  var s3BaseURL = 'https://'+Meteor.settings.AWS_REGION+'.amazonaws.com/'+Meteor.settings.AWS_BUCKET+'/user/';
+  var s3BaseURL = 'https://s3.amazonaws.com/'+Meteor.settings.AWS_BUCKET+'/user/';
   Meteor._powerQ.add(function(done){
     console.log('#: ', count_jobs, ' : ', user.profile.id);
     done();
