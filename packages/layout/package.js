a97e17cb4bfa9ebb095fdd0dce06271a92d5360b
Dave Bascom <dave@cumulocreative.com>
2015-01-19 09:00:50 -0500

    Add front end navbar status and notifications

--- packages/layout/package.js
+++ packages/layout/package.js
@@ -5,14 +5,17 @@ Package.describe({
 Package.onUse(function (api) {
   api.use([
     'templating', 'authorization', 'less',
-    'styles', 'jquery', 'stevezhu:velocity.js@0.1.0'
+    'styles', 'jquery', 'stevezhu:velocity.js@0.1.0',
+    'tracker', 'session'
     ], 'web');
 
   api.addFiles([
     'application.html', 'application_client.js',
     'navbar.html', 'navbar_client.js', 'navbar.less',
     'left_nav.html', 'leftNav.js', 'left_nav_client.js',
-    'left_nav.less'
+    'left_nav.less',
+
+    'notifications.html', 'notifications.less', 'notifications_client.js'
     ], 'web');
   
 });
