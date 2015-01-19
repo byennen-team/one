fe42f670c5942dfec6598e5a49bbe3bb6924d06c
Dave Bascom <dave@cumulocreative.com>
2015-01-19 11:19:19 -0500

    Cleaned up jshint errors

--- packages/layout/notifications_client.js
+++ packages/layout/notifications_client.js
@@ -1,7 +1,7 @@
 Template.notifications.rendered = function () {
   Session.set( 'dropdown', false );
   Session.set( 'statusPanel', true );
-}
+};
 
 Template.notifications.events({
 
@@ -9,11 +9,9 @@ Template.notifications.events({
   'click #notifications-avatar': function () {
      var open = Session.get( 'dropdown' );
     if( open ){
-  console.log('open - flash')    
       $( '#notification-dropdown' ).velocity("slideUp", { duration: 300 });
       Session.set( 'dropdown', false );
     } else {
-  console.log('close - flash')
       $( '#notification-dropdown' ).velocity("slideDown", { duration: 500, easing: "ease-out" });
       Session.set( 'dropdown', true );
     }
@@ -36,15 +34,9 @@ Tracker.autorun(function () {
   // otherwise show the notifications
   var statusPanel = Session.get( 'statusPanel' );
   if( statusPanel ){
-    console.log('true')
-    // $( '#status-panel' ).velocity({ opacity: 1 }, { display: "block" });
-    // $( '#notification-panel' ).velocity({ opacity: 0 }, { display: "none" });
     $( '#status-panel' ).velocity("slideDown", { duration: 500, easing: "ease-out", delay: 450 });
     $( '#notification-panel' ).velocity("slideUp", { duration: 500 });
   } else {
-    console.log('false')
-    // $( '#notification-panel' ).velocity({ opacity: 1 }, { display: "block" });
-    // $( '#status-panel' ).velocity({ opacity: 0 }, { display: "none" });
     $( '#notification-panel' ).velocity("slideDown", { duration: 500, easing: "ease-out", delay: 450 });
     $( '#status-panel' ).velocity("slideUp", { duration: 500 });
   }
