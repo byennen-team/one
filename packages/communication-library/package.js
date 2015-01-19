5d9f3d1d530bbe273790f4204e9044da9304116d
Lance Ennen <byennen@gmail.com>
2015-01-19 14:47:29 -0500

    move custom scrollbar package inside the packages that use them //cc @DaveSauce

--- packages/communication-library/package.js
+++ packages/communication-library/package.js
@@ -6,17 +6,27 @@ Package.onUse(function (api) {
   var both = ['web', 'server'];
 
   api.use([
-    'less', 'jquery', 'session',
-    'templating', 'mrt:moment', 'stevezhu:velocity.js@0.1.0', 
-    'routes', 'styles'
+    'less',
+    'jquery',
+    'session',
+    'templating',
+    'mrt:moment',
+    'stevezhu:velocity.js@0.1.0',
+    'routes',
+    'styles',
+    'maazalik:malihu-jquery-custom-scrollbar'
     ], 'web');
-  api.use('files', both);
 
   api.addFiles([
-    'communication_library_board.html', 'communication_library_board.less', 'communication_library_board_client.js',
-    'communication_library_members.html', 'communication_library_members.less', 'communication_library_members_client.js',
-    'communication_library_files.html', 'communication_library_files.less', 'communication_library_files_client.js'
- 
+    'communication_library_board.html',
+    'communication_library_board.less',
+    'communication_library_board_client.js',
+    'communication_library_members.html',
+    'communication_library_members.less',
+    'communication_library_members_client.js',
+    'communication_library_files.html',
+    'communication_library_files.less',
+    'communication_library_files_client.js'
   ], 'web');
 
-}); 
+});
