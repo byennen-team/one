5d15c27ed0d3a93a99eb1e52e4b9aa33c210b8d7
Lance Ennen <byennen@gmail.com>
2015-01-19 14:09:40 -0500

    fix dashboard package remove old demo code

--- packages/dashboard/dashboard_client.js
+++
@@ -1,8 +0,0 @@
-Template.dashboard.events({
-  'click #chat-button': function (event, template) {
-    $('.chat-window').velocity({ bottom: 0 }, "easeInSine");
-  },
-  'click .chat-window': function (event, template) {
-    $('.chat-window').velocity({ bottom: -1000 }, "easeInSine");
-  }
-});
--- packages/dashboard/package.js
+++ packages/dashboard/package.js
@@ -5,7 +5,10 @@ Package.describe({
 Package.onUse(function (api) {
   api.use(['templating', 'less', 'mrt:moment'], 'web');
   api.addFiles([
-    'dashboard.html', 'dashboard_client.js', 'dashboard.less',
-    'dashboard_banner.html', 'dashboard_banner_client.js', 'dashboard_banner.less'
+    'dashboard.html',
+    'dashboard.less',
+    'dashboard_banner.html',
+    'dashboard_banner_client.js',
+    'dashboard_banner.less'
     ], 'web');
 });
