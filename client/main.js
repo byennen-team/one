d7e7fd54f12b5bad79fb761a9260734265014ddc
Chet Corcos <ccorcos@gmail.com>
2015-01-22 23:58:52 -0800

    non reversible transitions between items in the left nav

--- client/main.js
+++ client/main.js
@@ -1,1 +1,10 @@
-Transitioner.transition('login', 'dashboard', 'slideUp');
+Transitioner.transition(Routes.LOGIN, Routes.DASHBOARD, 'slideUp');
+
+
+
+Transitioner.transition(Routes.DASHBOARD, Routes.DIRECTORY, 'slideRight', {reverse: false});
+Transitioner.transition(Routes.DASHBOARD, Routes.APPS, 'slideRight', {reverse: false});
+Transitioner.transition(Routes.DIRECTORY, Routes.DASHBOARD, 'slideRight', {reverse: false});
+Transitioner.transition(Routes.DIRECTORY, Routes.APPS, 'slideRight', {reverse: false});
+Transitioner.transition(Routes.APPS, Routes.DIRECTORY, 'slideRight', {reverse: false});
+Transitioner.transition(Routes.APPS, Routes.DASHBOARD, 'slideRight', {reverse: false});
