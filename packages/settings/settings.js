73801fd3dc313a1a2d929d7d5ffd021a10a0b229
Chet Corcos <ccorcos@gmail.com>
2015-01-22 22:07:15 -0800

    added a simple settings check from crater io pro tip

--- packages/settings/settings.js
+++ packages/settings/settings.js
@@ -1,5 +1,14 @@
 /* global Settings: true */
 
+// lets complain to server command line if you forgot --settings when running Meteor
+if (Meteor.isServer) {  
+  if (Object.keys(Meteor.settings).length === 0) {
+    console.log("You forgot to run Meteor with --settings!");
+    console.log("Try this: meteor --settings settings.development.json")
+    console.log ("")
+  }
+}
+
 Settings = {};
 
 Settings.isDevelopment = Meteor.settings.public.ENVIRONMENT === 'development';
