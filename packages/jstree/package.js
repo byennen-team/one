a7da85ed7fee88c2efa71767727ad8126029d90b
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-01-27 00:43:02 +0100

    Include jstree directly to be able to modify the theme

--- packages/jstree/package.js
+++ packages/jstree/package.js
@@ -7,6 +7,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
-  api.use('mquandalle:bower', 'web');
-  api.addFiles('smart.json', 'web');
+  api.addFiles([
+    'dist/jstree.js',
+    'dist/themes/default/style.css',
+    'dist/themes/default/32px.png',
+    'dist/themes/default/40px.png',
+    'dist/themes/default/throbber.gif'
+  ], 'web');
 });
