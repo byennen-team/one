6a5c91904252b6a4514a1cb446b7f18cbc20d818
Chuck Short <chuck.short.smart@gmail.com>
2015-01-30 13:16:29 -0500

    login.js conflict

--- packages/one-login/login.js
+++ packages/one-login/login.js
@@ -15,11 +15,6 @@ Template.login.events({
     var agentEmail = template.find('#agentEmail').value;
     agentId = parseInt(agentId);
     Meteor.loginWithElliman(agentId, agentEmail, function (error) {
-<<<<<<< HEAD
-      console.assert(!error, 'error', error); return;
-=======
-      // console.log('error', error); return;
->>>>>>> b245f3b27448f0d1ce1f8aa16dbe810b9653b437
       //TODO: better error messages. Maybe setup flash notications.
       if (error) {
         template.error.set(error.error);
