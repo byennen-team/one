909f3e7f3a5aa3f461c07ea164f61f602fa2b45a
Chuck Short <chuck.short.smart@gmail.com>
2015-02-01 15:25:26 -0500

    resize images in startup sequence through lambda

--- packages/one-account-settings/account_settings_server.js
+++ packages/one-account-settings/account_settings_server.js
@@ -2,53 +2,6 @@ Meteor.methods({
   /**
    * Create a signature to upload the current user's profile picture to s3.
    */
-  resizeNewProfileImage: function(newUrl) {
-      check(newUrl, String);
-      var user = Meteor.user();
-      var _ext = '.'+FileTools.ext(newUrl);
-      Meteor._powerQ.pause();
-      Meteor._powerQ.add(function(done) {
-        FileTools.fetchToTemp(newUrl, done);
-      });
-      Meteor._powerQ.add(function(done) {
-        FileTools.resizeTemp('thumb_', done);
-      });
-      Meteor._powerQ.add(function(done){
-        FileTools.upload(
-          'thumb_',
-          '/user/'+user.profile.id+'/profile-images/thumb_'+user.profile.id,
-          done
-        );
-      });
-      Meteor._powerQ.add(function(done) {
-        FileTools.resizeTemp('full_', done);
-      });
-      Meteor._powerQ.add(function(done){
-        FileTools.upload(
-          'full_',
-          '/user/'+user.profile.id+'/profile-images/full_'+user.profile.id,
-          done
-        );
-      });
-      Meteor._powerQ.add(function(done) {
-      var largeUrlRaw = 'user/'+user.profile.id+
-        '/profile-images/full_'+user.profile.id+_ext;
-      var thumbUrlRaw = 'user/'+user.profile.id+
-        '/profile-images/thumb_'+user.profile.id+_ext;
-      var thumbSigned = FileTools.signedGet(thumbUrlRaw);
-      var largeSigned = FileTools.signedGet(largeUrlRaw);
-    user.profile.photoUrl = {
-        large: largeSigned,
-        thumb: thumbSigned
-    };
-    Meteor.users.update(
-      user._id,
-      { "$set": { "profile.photoUrl": user.profile.photoUrl }}
-    );
-    done();
-    });
-    Meteor._powerQ.resume();
-  },
   s3Signature: function (fileName, mimeType, gallery, bucket) {
     check(fileName, String);
     check(mimeType, String);
