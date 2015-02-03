aebb409e3b1c9aab26005017e1a9d993683e54d6
Dave Bascom <dave@cumulocreative.com>
2015-02-03 13:34:59 -0500

    first pass at welcome messages

--- packages/one-communication/communication_message_board_client.js
+++ packages/one-communication/communication_message_board_client.js
@@ -1,5 +1,13 @@
-Template.messageBoard.events({
+Template.messageBoard.helpers({
 
+// TODO: if current channel is a task return ture, else return false
+  taskChannel: function () {
+    return true;
+  },
 
+// TODO: if current channel is a room return ture, else return false
+  roomChannel: function () {
+    return false;
+  }
 
 });
