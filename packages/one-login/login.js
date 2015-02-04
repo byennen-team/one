979f9f8d59087f93c57b44aa4b03eb988806f7d6
Chuck Short <chuck.short.smart@gmail.com>
2015-02-04 12:05:57 -0500

    fix console.assert conflict

--- packages/one-login/login.js
+++ packages/one-login/login.js
@@ -15,7 +15,6 @@ Template.login.events({
     var agentEmail = template.find('#agentEmail').value;
     agentId = parseInt(agentId);
     Meteor.loginWithElliman(agentId, agentEmail, function (error) {
-      console.assert(!error, 'error', error); return;
       //TODO: better error messages. Maybe setup flash notications.
       if (error) {
         template.error.set(error.error);
