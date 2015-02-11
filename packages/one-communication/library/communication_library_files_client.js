c98739300faeb84e228911983f32a40ed767ba95
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-02-11 15:04:00 +0100

    libraryFiles category selection

--- packages/one-communication/library/communication_library_files_client.js
+++ packages/one-communication/library/communication_library_files_client.js
@@ -1,5 +1,18 @@
+Template.libraryFiles.created = function () {
+  // Template level subscription for files
+  /*
+  this.autorun(function () {
+    if (Session.get('libraryFilesSelectedCategory') === 'Company Docs') {
+      // TODO: Subscribe to company docs
+    } else {
+      // TODO: Subscribe to my docs
+    }
+  });
+  */
+};
+
 Template.libraryFiles.rendered = function(){
-  $(".library-files-sleeve").mCustomScrollbar({
+  this.$(".library-files-sleeve").mCustomScrollbar({
   	theme:"one-light",
   	scrollbarPosition: "inside"
   });
@@ -8,7 +21,33 @@ Template.libraryFiles.rendered = function(){
 Template.libraryFiles.helpers({
 
   documents: function () {
-    return Files.find();
+    if (Session.get('libraryFilesSelectedCategory') === 'Company Docs') {
+      return Files.find(
+        {companyDocument: true}
+      );
+    } else {
+      return Files.find(
+        {companyDocument: false}
+      );
+    }
+  },
+
+  selectedCategory: function () {
+    return Session.get('libraryFilesSelectedCategory');
+  }
+
+});
+
+Template.libraryFiles.events({
+
+  'click [data-action="select-my-docs"]': function (event) {
+    event.preventDefault();
+    Session.set('libraryFilesSelectedCategory', 'My Docs');
+  },
+
+  'click [data-action="select-company-docs"]': function (event) {
+    event.preventDefault();
+    Session.set('libraryFilesSelectedCategory', 'Company Docs');
   }
 
 });
