27f96796723487942c84985894ff3d56a28b3a91
Dave Bascom <dave@cumulocreative.com>
2015-02-19 16:40:18 -0500

    Mobile internal profile complete

--- packages/one-profile/lib/client/modals/profile_gallery_mobile_controls_client.js
+++ packages/one-profile/lib/client/modals/profile_gallery_mobile_controls_client.js
@@ -1,7 +1,7 @@
 Template.profileGalleryMobileControls.events({
 
   // Closes mobile gallery controls modal
-  'click #profile-gallery-mobile-cancel': function () {
+  'click #profile-gallery-mobile-cancel, click .control-panel > li': function(){
     $( "#profile-gallery-mobile-controls" )
       .velocity( "slideUp", { duration: 500 } );
     $( "#profile-screen" ).velocity( "fadeOut", { duration: 500 } );
